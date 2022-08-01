82e98134d3468228a439b319862bcbcbe805bfa8
KyleP <kyl3p3tty@gmail.com>
2022-07-31 18:39:56 -0700

    server file structure

---
+++ server/schemas/resolvers.js
@@ -0,0 +1,9 @@
+const { AuthenticationError } = require('apollo-server-express');
+const { signToken } = require('../utils/auth');
+const stripe = require('stripe')('');
+
+const resolvers = {
+  
+};
+
+module.exports = resolvers;
