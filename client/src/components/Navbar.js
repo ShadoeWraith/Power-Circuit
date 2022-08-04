6c2840955bfbe49641b15e25a994ac48d6c860a8
Shaun Kiszonas <shadoewraith99@gmail.com>
2022-08-04 12:39:09 -0700

    navbar v1

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,11 +1,35 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faShoppingCart,
+  faMagnifyingGlass,
+} from "@fortawesome/free-solid-svg-icons";
+
 export default function Navbar() {
   return (
     <nav>
-      <div className="nav-header">
+      <div className="nav-top">
         <h1 className="nav-title">Krafty</h1>
+        <div className="search-bar">
+          <input type="text" placeholder="Search for anything"></input>
+          <FontAwesomeIcon className="search-icon" icon={faMagnifyingGlass} />
+        </div>
+        <div className="nav-buttons">
+          <button type="button">Sign In</button>
+          <button>
+            <FontAwesomeIcon icon={faShoppingCart} />
+          </button>
+        </div>
       </div>
       <ul className="nav-categories">
-        <li>Category</li>
+        <li>Home Favorites</li>
+        <li>Jewlery &amp; Accessories</li>
+        <li>Clothing &amp; Shoes</li>
+        <li>Home &amp; Living</li>
+        <li>Wedding &amp; Party</li>
+        <li>Toys &amp; Entertainment</li>
+        <li>Art &amp; Collectibles</li>
+        <li>Craft Supplies</li>
+        <li>Gifts &amp; Gift Cards</li>
       </ul>
     </nav>
   );
--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <section>
-      <h1>Hello</h1>
+      <h1 className="page-header">Hello</h1>
     </section>
   );
 }
