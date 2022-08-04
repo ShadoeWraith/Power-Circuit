aff992e979c824f5b72a489dbed9019514af8425
KyleP <kyl3p3tty@gmail.com>
2022-08-04 13:55:17 -0700

    added mysql framework

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,8 +1,12 @@
-const mongoose = require('mongoose');
+const Sequelize = require('sequelize');
+require('dotenv').config();
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/mernshopping', {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-});
+const sequelize = process.env.JAWSDB_URL
+  ? new Sequelize(process.env.JAWSDB_URL)
+  : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
+      host: 'localhost',
+      dialect: 'mysql',
+      port: 3306
+    });
 
-module.exports = mongoose.connection;
+module.exports = sequelize;
