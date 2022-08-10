e32ab9acebd166ebed505725561d8eb96774e3fe
Shaun Kiszonas <shadoewraith99@gmail.com>
2022-08-10 11:45:12 -0700

    added link to card

--- client/src/components/Card.js
+++ client/src/components/Card.js
@@ -3,12 +3,18 @@ import graphicsCardImg from "../assets/graphics-card.jpg";
 export default function Card() {
   return (
     <div className="card-container">
-      <div className="card">
-        <img className="card-image" src={graphicsCardImg} alt="graphics card" />
-        <div className="card-text">
-          <h3 className="card-title">Graphics Cards</h3>
+      <a href="/">
+        <div className="card">
+          <img
+            className="card-image"
+            src={graphicsCardImg}
+            alt="graphics card"
+          />
+          <div className="card-text">
+            <h3 className="card-title">Graphics Cards</h3>
+          </div>
         </div>
-      </div>
+      </a>
     </div>
   );
 }
