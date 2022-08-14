09da42ab9d3ec1fc16faf5f1c21e2548b651e537
Shaun Kiszonas <shadoewraith99@gmail.com>
2022-08-13 22:07:30 -0700

    added carousel

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,8 +1,10 @@
+import Image from "../components/Carousel";
 import Card from "../components/Card";
 
 export default function Home() {
   return (
     <section>
+      <Image />
       <h1 className="page-header"> Popular products </h1>
       <Card />
     </section>
