721019198743516323dc48809e39cfd8fec86e47
Shaun Kiszonas <shadoewraith99@gmail.com>
2022-08-21 20:02:28 -0700

    added featured products section

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,13 +1,18 @@
 import Image from "../components/Carousel/Carousel";
 import Card from "../components/Card/Card";
+import Product from "../components/Product/Product";
 
 export default function Home() {
   return (
     <section>
       {/* Image = Carousel */}
-      <Image />
-      <h1 className="page-header"> Popular products </h1>
+      {/* <Image /> */}
+      <h1 className="page-header"> Popular categories </h1>
       <Card />
+
+      <div className="product-section">
+        <Product />
+      </div>
     </section>
   );
 }
